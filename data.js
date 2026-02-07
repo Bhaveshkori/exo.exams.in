e5746c7116e4bdba2aa6b41723d69c9ffe03bccc
Bhaveshkori <143033305+Bhaveshkori@users.noreply.github.com>
2026-02-07 17:42:54 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,29 +1,10 @@
-"JEE Advanced": [
-   { year: "2025", file: "pepar/Ch26.pdf" }
-]
-
 const exams = {
   "jee": {
-    name: "JEE Advanced",
+    name: "JEE main ",
     years: {
-      "2024": "pepar/jee/2024.pdf",
-      "2023": "pepar/jee/2023.pdf",
-      "2022": "pepar/jee/2022.pdf"
-    }
-  },
-
-  "neet": {
-    name: "NEET UG",
-    years: {
-      "2024": "pepar/neet/2024.pdf",
-      "2023": "pepar/neet/2023.pdf"
-    }
-  },
-
-  "upsc": {
-    name: "UPSC CSE",
-    years: {
-      "2024": "pepar/upsc/2024.pdf"
+      "2024": "paper/jee/2024.pdf",
+      "2023": "paper/jee/2023.pdf",
+      "2022": "paper/jee/2022.pdf"
     }
   }
 };
