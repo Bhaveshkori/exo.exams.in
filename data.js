a5d4f7ce3256f82b477cb8a092b4dceb016903d9
Bhaveshkori <143033305+Bhaveshkori@users.noreply.github.com>
2026-02-07 18:17:15 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,19 @@
 const exams = {
-  "jee": {
-    name: "JEE main ",
+  jee: {
+    name: "JEE main",
     years: {
-      "2024": "paper/jee/2024.pdf",
-      "2023": "paper/jee/2023.pdf",
-      "2022": "paper/jee/2022.pdf"
+      "2024": "pepar/jee/2024.pdf",
+      "2023": "pepar/jee/2023.pdf",
+      "2022": "pepar/jee/2022.pdf"
+   }
+  },
+
+  neet: {
+    name: "NEET",
+    years: {
+      "2024": "pepar/neet/2024.pdf",
+      "2023": "pepar/neet/2023.pdf",
+      "2022": "pepar/neet/2022.pdf"
     }
-  }
-};
+  };
+
